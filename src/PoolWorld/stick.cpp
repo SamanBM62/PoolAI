312def39b09978c01f11c45d17532a53fca83a81
Saman <m28mahmoodi@gmail.com>
2025-03-02 19:41:45 +0100

    less magical numbery

--- src/PoolWorld/stick.cpp
+++ src/PoolWorld/stick.cpp
@@ -34,24 +34,20 @@ void Stick::moveStick(const sf::Vector2f& ballPos, float mouseX, float mouseY) {
     auto differenceX = ballPos.x - mouseX;
     auto differenceY = ballPos.y - mouseY;
 
-    // Calculate the magnitude (distance) between the ball and the mouse
     float magnitude = std::sqrt(differenceX * differenceX + differenceY * differenceY);
     
     sf::Vector2f direction(mouseX - ballPos.x, mouseY - ballPos.y);
 
-    // If the magnitude is greater than 500, scale the direction vector to 500 pixels
-    if (magnitude > 100.f) {
-        // Normalize the vector and scale it to 500 pixels
-        direction = direction / magnitude * 100.f;
+    float maxDistancePixel = 200.f;
+
+    if (magnitude > maxDistancePixel) {
+        direction = direction / magnitude * maxDistancePixel;
     }
 
-    // Set the position of the stick to be 500 pixels away from the ball, in the direction of the mouse
     sf::Vector2f stickPosition = ballPos + direction;
 
-    // Calculate the angle using the direction vector
     float angleInRadians = std::atan2(direction.y, direction.x);
 
-    // Convert to sf::Angle
     sf::Angle angle = sf::radians(angleInRadians);
 
     // Set the position and rotation of the stick
